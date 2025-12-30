4f93d455e23311994efc89a54b9a6069c30c473e
Ain Farhana <nufahana05@gmail.com>
2025-12-31 01:06:15 +0800

    responsive

--- frontend/src/components/Navbar/Navbar.js
+++ frontend/src/components/Navbar/Navbar.js
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, {useState} from 'react';
+import { RiMenu3Line, RiCloseLine } from 'react-icons/ri';
 import './Navbar.css';
 
 function Navbar({onConsultClick}) {
+  const [toggleMenu, setToggleMenu] = useState(false);
   return (
     <nav className="navbar">
       <div className="navbar-container">
@@ -26,8 +28,28 @@ function Navbar({onConsultClick}) {
           <button className="btn-consult" onClick={onConsultClick}>Consult Now</button>
           <div className="user-profile">
             <span className="user-icon">👤</span>
+
+              {/* Mobile Menu Icon */}
+              <div className="mobile-menu-icon">
+                {toggleMenu 
+                  ? <RiCloseLine color="#003366" size={27} onClick={() => setToggleMenu(false)} />
+                  : <RiMenu3Line color="#003366" size={27} onClick={() => setToggleMenu(true)} />
+                }
+              </div>
           </div>
         </div>
+
+        {/* Mobile Overlay Menu */}
+        {toggleMenu && (
+          <div className="nav-menu-mobile">
+            <ul className="nav-links-mobile">
+              <li><a href="#about-section" onClick={() => setToggleMenu(false)}>About</a></li>
+              <li><a href="#services-section" onClick={() => setToggleMenu(false)}>Services</a></li>
+              <li><a href="#portfolio" onClick={() => setToggleMenu(false)}>Portfolio</a></li>
+            </ul>
+          </div>
+        )}
+
       </div>
     </nav>
   );
